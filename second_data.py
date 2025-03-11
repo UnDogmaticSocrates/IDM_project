99ed5c2657dc127a4163c67279ad89d0f9c494b5
UnDogmaticSocrates <jorgevera.utt@gmail.com>
2025-03-11 18:49:27 +0000

    final

--- second_data.py
+++ second_data.py
@@ -2,7 +2,7 @@ import pandas as pd
 import openpyxl
 import os
 #ruta de carpeta
-carpeta_cotizaciones = "/workspaces/IDM_project/cotizaciones"
+carpeta_cotizaciones = "/workspaces/IDM_project/folder_test"
 archivo_base = "control_de_facturacion_2025.xlsx"
 
 if os.path.exists(archivo_base):
@@ -16,31 +16,24 @@ for archivo in os.listdir(carpeta_cotizaciones):
         ruta_archivo = os.path.join(carpeta_cotizaciones, archivo)
 
         wb = openpyxl.load_workbook(ruta_archivo, data_only=True)
-        sheet = wb["Cotización"]  # Ajusta el nombre de la hoja si es diferente
+        sheet = wb["Cotizacion"]  # Ajusta el nombre de la hoja si es diferente
 
         # 🔎 Extraer datos según las coordenadas que mencionaste
+        po = sheet["K3"].value if sheet["K3"].value else "No encontrado"
+        fecha_po = sheet["K4"].value if sheet["K4"].value else "No encontrado"
+        no_cotizacion = sheet["K5"].value if sheet["K5"].value else "No encontrado"
         empresa = sheet["B3"].value if sheet["B3"].value else "No encontrado"
         requisitor = sheet["B5"].value if sheet["B5"].value else "No encontrado"
         descripcion = sheet["B15"].value if sheet["B15"].value else "No encontrado"
-        no_cotizacion = sheet["K5"].value if sheet["K5"].value else "No encontrado"
         cantidad = sheet["H15"].value if sheet["H15"].value else "No encontrado"
-        po = sheet["K3"].value if sheet["K3"].value else "No encontrado"
-        fecha_po = sheet["K4"].value if sheet["K4"].value else "No encontrado"
+        tipo_moneda = sheet["K15"].value if sheet["K15"].value else "No encontrado"
         precio_unidad = sheet["J15"].value if sheet["J15"].value else "No encontrado"
         subtotal = sheet["L36"].value if sheet["L36"].value else "No encontrado"
-        total = sheet["L38"].value if sheet["L38"].value else "No encontrado"
         iva = sheet["L37"].value if sheet["L37"].value else "No encontrado"
-
-
-
-
-
-
-
-        
-
-        nueva_fila = pd.DataFrame([[archivo, empresa, requisitor, no_cotizacion, cantidad, descripcion]], 
-                                  columns=["Archivo", "Empresa", "Requisitor", "No. Cotización", "Cantidad", "Descripción"])
+        total = sheet["L38"].value if sheet["L38"].value else "No encontrado"
+    
+        nueva_fila = pd.DataFrame([[archivo, empresa, requisitor, no_cotizacion, cantidad, descripcion, po, fecha_po, precio_unidad, subtotal, iva, total, tipo_moneda]], 
+                                  columns=["Archivo", "Empresa", "Requisitor", "No. Cotización", "Cantidad", "Descripción", "Po", "Fecha de Po", "Precio Unitario", "Subtotal", "IVA", "Total", "Tipo de moneda"])
         
          # Agregar a la base de datos
         df_base = pd.concat([df_base, nueva_fila], ignore_index=True)
